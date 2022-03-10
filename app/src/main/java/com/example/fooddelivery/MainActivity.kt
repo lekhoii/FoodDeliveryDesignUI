76714e6f48ab86162cc97edd8a653bd6736d7e0e
TuNguyen227 <nmtu2272000@gmail.com>
2022-03-10 21:58:11 +0700

    Food Delivery Design UI

---
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fooddelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide();
+        setContentView(R.layout.signup)
+    }
+}
